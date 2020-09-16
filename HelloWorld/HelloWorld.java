9e6d11e61b5d390d30ca76548ba8bb2c2f2d104a
FridericHansel <friderichansel@gmail.com>
2020-09-16 11:00:43 +0700

    HelloWorld.java

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+	public static void main (String[] args) {
+		System.out.println("Nama:Frideric Hansel");
+		System.out.println("Kelas:D3IF44-04");
+		System.out.println("NIM:6706201012");
+	}
+}
